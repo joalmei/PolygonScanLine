ca61c6b35fc3063d8e5a04b2be833a9786d639f2
gugation <33260223+gugation@users.noreply.github.com>
2019-04-10 23:16:15 -0300

    Update activeedgetable.cpp

--- activeedgetable.cpp
+++ activeedgetable.cpp
@@ -1,50 +1,69 @@
 #include "activeedgetable.h"
 
 ActiveEdgeTable::ActiveEdgeTable(vector<QPoint> Vertices) : ET(Vertices){
-    AET.resize(ALTURA);
+
 }
 
 void ActiveEdgeTable::DrawAETMethod(QPainter& painter){
     size_t i = 0;
+
     while(i < ALTURA){
+
+        //cout << "Nivel " << i << endl;
+
         //Transfere o cesto i da ET na AET e ordena
-        /*
-        if(ET.NotEmpty[i]){
-            for(size_t j = 0; j < ET.ET[i].size(); j++){
-                AET[static_cast<size_t>(ET.NotEmpty[i])].push_back(ET.ET[i][j]);
-                sort(AET[i].begin(), AET[i].end());
+        if(!ET.IsEmpty[i]){
+            for(size_t j = 0; j < ET.Tabela[i].size(); j++){
+                AET.push_back(ET.Tabela[i][j]);
+                sort(AET.begin(), AET.end());
             }
-        }/*
+        }
+
+        //for(size_t j = 0; j < AET.size(); j++)
+            //cout << "Ymax = " << AET[j].get_ymax() << " X = " << AET[j].get_x() << " m = " << AET[j].get_m() << endl;
+
         //Retira os lados y = ymax
-        for(size_t j = 0; j < AET[i].size(); j++){
-            if(AET[i][j].get_ymax() == static_cast<int>(i)){
-                for(size_t k = j; k < AET[i].size() - 1; k++)
-                    AET[i][j] = AET[i][j + 1];
-                AET[i].pop_back();
+        for(size_t j = 0; j < AET.size(); j++){
+            if(AET[j].get_ymax() == static_cast<int>(i)){
+                if(AET.size() == 1){
+                    AET.pop_back();
+                }
+                else{
+                for(size_t k = j; k < AET.size() - 1; k++)
+                    AET[j] = AET[j + 1];
+                AET.pop_back();
+                j--;
+                }
             }
         }
         //Desenha os pixels na linha de varredura y usando pares de coordenadas x da AET
-        if(AET[i].size() == 1){
-            painter.drawPoint(FloatIntegerAdaptation(AET[i][0].get_x()), static_cast<int>(i));
+        if(AET.size() == 1){
+            painter.drawPoint(FloatIntegerAdaptation(AET[0].get_x()), static_cast<int>(i));
+            //cout << FloatIntegerAdaptation(AET[0].get_x()) << " " << static_cast<int>(i) << endl;
         }
-        else if(AET[i].size() > 1){
-            for(size_t j = 0; j < AET[i].size() - 1; j++){
-                for(int k = FloatIntegerAdaptation(AET[i][j].get_x()); k < AET[i][j + 1].get_x(); k++){
+        else if(AET.size() > 1){
+            for(size_t j = 0; j < AET.size() - 1; j++){
+                for(int k = FloatIntegerAdaptation(AET[j].get_x()); k < AET[j + 1].get_x(); k++){
                     painter.drawPoint(k, static_cast<int>(i));
+                    //cout << "Grade [" << k << " " << static_cast<int>(i) << ")" << endl;
                 }
             }
         }
         //Incrementa y de 1 (no caso y = i)
         i++;
+        //cout << "to vivo!\n";
         //Para cada aresta nao vertical atualiza x = x + m
-        for(size_t k = 0; k < AET[i].size(); k++){
-            BlocoET b(AET[i][k].get_ymax(), AET[i][k].get_x() + AET[i][k].get_m(), AET[i][k].get_m());
-            AET[i + 1].push_back(b);
+        for(size_t k = 0; k < AET.size(); k++){
+            BlocoET b(AET[k].get_ymax(), AET[k].get_x() + AET[k].get_m(), AET[k].get_m());
+            AET[k] = b;
         }
+        //cout << "arebaba\n";
         //Reordena a AET
-        sort(AET[i + 1].begin(), AET[i + 1].end());
-*/i++;
+        sort(AET.begin(), AET.end());
+
+        //i++;
     }
+
 }
 
 //Funcao para retornar x+1 caso seja float e x caso seja inteiro
