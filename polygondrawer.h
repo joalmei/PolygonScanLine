073de8e3fdb6675a6785a87e6b791602defa1899
gugation <33260223+gugation@users.noreply.github.com>
2019-04-10 23:18:54 -0300

    Update polygondrawer.h

--- polygondrawer.h
+++ polygondrawer.h
@@ -6,7 +6,8 @@ class CanvasOpenGL;
 #include "drawer.h"
 #include "qcolor.h"
 #include "qpoint.h"
-#include "qpainter.h"
+//#include "qpainter.h" incluso no activeedgetable.h
+#include "activeedgetable.h"
 
 class PolygonDrawer : public Drawer
 {
@@ -17,10 +18,12 @@ public:
     PolygonDrawer(CanvasOpenGL* canvas);
     virtual ~PolygonDrawer();
     void Draw(QColor pointsColor);
+    void AETMethod(vector<QPoint> points, QPainter& painter);
 
 private:
     void oddEvenFillMethod(std::vector<QPoint*>& vertices, QPainter& painter);
-
 };
+
+
 
 #endif // POLYGONDRAWER_H
