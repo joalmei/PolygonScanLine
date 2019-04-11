698594732d8344f327248b5bf1f48444f2f4f5e0
gugation <33260223+gugation@users.noreply.github.com>
2019-04-11 11:34:44 -0300

    Update activeedgetable.cpp

--- activeedgetable.cpp
+++ activeedgetable.cpp
@@ -9,8 +9,6 @@ void ActiveEdgeTable::DrawAETMethod(QPainter& painter){
 
     while(i < ALTURA){
 
-        //cout << "Nivel " << i << endl;
-
         //Transfere o cesto i da ET na AET e ordena
         if(!ET.IsEmpty[i]){
             for(size_t j = 0; j < ET.Tabela[i].size(); j++){
@@ -18,9 +16,6 @@ void ActiveEdgeTable::DrawAETMethod(QPainter& painter){
                 sort(AET.begin(), AET.end());
             }
         }
-
-        //for(size_t j = 0; j < AET.size(); j++)
-            //cout << "Ymax = " << AET[j].get_ymax() << " X = " << AET[j].get_x() << " m = " << AET[j].get_m() << endl;
 
         //Retira os lados y = ymax
         for(size_t j = 0; j < AET.size(); j++){
@@ -39,29 +34,23 @@ void ActiveEdgeTable::DrawAETMethod(QPainter& painter){
         //Desenha os pixels na linha de varredura y usando pares de coordenadas x da AET
         if(AET.size() == 1){
             painter.drawPoint(FloatIntegerAdaptation(AET[0].get_x()), static_cast<int>(i));
-            //cout << FloatIntegerAdaptation(AET[0].get_x()) << " " << static_cast<int>(i) << endl;
         }
         else if(AET.size() > 1){
             for(size_t j = 0; j < AET.size() - 1; j++){
                 for(int k = FloatIntegerAdaptation(AET[j].get_x()); k < FloatIntegerAdaptation(AET[j + 1].get_x()); k++){
                     painter.drawPoint(k, static_cast<int>(i));
-                    //cout << "Grade [" << k << " " << static_cast<int>(i) << ")" << endl;
                 }
             }
         }
         //Incrementa y de 1 (no caso y = i)
         i++;
-        //cout << "to vivo!\n";
         //Para cada aresta nao vertical atualiza x = x + m
         for(size_t k = 0; k < AET.size(); k++){
             BlocoET b(AET[k].get_ymax(), AET[k].get_x() + AET[k].get_m(), AET[k].get_m());
             AET[k] = b;
         }
-        //cout << "arebaba\n";
         //Reordena a AET
         sort(AET.begin(), AET.end());
-
-        //i++;
     }
 
 }
