af1e2f8640875c7f45d648e5aca277187022e72d
gugation <33260223+gugation@users.noreply.github.com>
2019-04-14 21:51:20 -0300

    Update blocoet.cpp

--- blocoet.cpp
+++ blocoet.cpp
@@ -3,11 +3,11 @@
 BlocoET::BlocoET(int ymin, int ymax, int xmin, int xmax)
 {
     this->ymax = ymax;
-    this->x = static_cast<float>(xmin);
-    this->m = (static_cast<float>(xmax - xmin) / (ymax - ymin));
+    this->x = static_cast<double>(xmin);
+    this->m = (static_cast<double>(xmax - xmin) / (ymax - ymin));
 }
 
-BlocoET::BlocoET(int ymax, float x, float m){
+BlocoET::BlocoET(int ymax, double x, double m){
     this->ymax = ymax;
     this->x = x;
     this->m = m;
@@ -19,19 +19,4 @@ bool BlocoET::operator < (BlocoET obj){
     else {
         return false;
     }
-}
-
-int BlocoET::get_ymax(){
-    return ymax;
-}
-
-float BlocoET::get_x(){
-    return x;
-}
-float BlocoET::get_m(){
-    return m;
-}
-
-void BlocoET::set_x(float new_x){
-    x = new_x;
 }
